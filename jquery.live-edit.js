8fbc8a5e66a5a205b730498883023f36b2b22d63
U-tim-thinkpad\Tim Branyen <Tim Branyen@tim-thinkpad.(none)>
2010-03-31 23:48:49 -0400

    Initial commit

---
+++ jquery.live-edit.js
@@ -0,0 +1,9 @@
+(function jQuery_LiveEdit($, window, undefined) {
+    
+    $.fn.liveEdit = function liveEdit() {
+        var utility = {
+            
+        };
+    }
+    
+})(jQuery, window, undefined);
